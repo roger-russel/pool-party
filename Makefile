7ed12f9e91dbf9db9e3ab60de943a0a290b26f0e
Roger Russel <roger@rrussel.org>
2020-12-13 13:11:55 -0300

    Complete testes on queue

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@
 test:
 	@go test ./... -cover -coverprofile=./coverage.txt -covermode=atomic
 
+.PHONY: coverage
+coverage: test
+	@go tool cover -html=./coverage.txt -o coverage.html
+
--- pkg/pool/queue_test.go
+++ pkg/pool/queue_test.go
@@ -29,3 +29,42 @@ func Test_queueImpl_put(t *testing.T) {
 
 	})
 }
+
+func Test_queueImpl_Get(t *testing.T) {
+	t.Run("Get from Queue", func(t *testing.T) {
+		q := queueImpl{}
+
+		q.put(&workerImpl{
+			id:       "1",
+			queuedAt: time.Now(),
+			run: func() error {
+				return func(id string) error {
+					return nil
+				}("1")
+			},
+		})
+
+		if len(q.queued) != 1 {
+			t.Errorf("unexpeced size of queue: %d", len(q.queued))
+		}
+
+		if q.len() != 1 {
+			t.Errorf("queue dont looks like it increased lenth of queued workers, got: %d", q.len())
+		}
+
+		count := 0
+
+		w1 := q.get(&count)
+
+		if w1.ID() != "1" {
+			t.Errorf("unexpected id as received: %s", w1.ID())
+		}
+
+		w2 := q.get(&count)
+
+		if w2 != nil {
+			t.Errorf("unexpected worker from queue it was suposed to be empty but got: %+v", w2)
+		}
+
+	})
+}
